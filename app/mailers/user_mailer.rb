1dc0b559df568a392ce194affb9cb8f0df599038
Phan Hoang Hieu <phan.hoang.hieu@sun-asterisk.com>
2019-11-06 14:24:56 +0700

    Chapter_12

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,11 @@
 class UserMailer < ApplicationMailer
   def account_activation user
     @user = user
-    mail to: user.email, subject: t("user_mailer.Account_mail")
+    mail to: user.email, subject: I18n.t(".account_mail")
+  end
+
+  def password_reset user
+    @user = user
+    mail to: user.email, subject: I18n.t(".reset_password")
   end
 end
